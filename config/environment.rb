5973fc8ad3fdfd516ab49ae64178e5a04320e035
Jason <jasonkwong11@gmail.com>
2016-09-14 04:09:51 -0400

    updated db for heroku

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,26 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 configure :development do
-  set :database, "sqlite3:db/database.db"
+# set :database, 'sqlite:///dev.db'
+set :database, 'sqlite3:database.db'
+set :show_exceptions, true
+end
+
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+ ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
 end
 
 ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
+  :adapter => "postgresql",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
 
